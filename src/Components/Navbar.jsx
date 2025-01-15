e0cf5fbb3de7a02826846e35dca3fa77939c81b6
AshanMaduwantha <ashanofficial256@gmail.com>
2025-01-15 10:54:45 +0530

    edit some code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const Navbar = () => {
   return (
     <div className='absolute top-0 left-0 w-full z-10'>
-      <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+      <div className='container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-3 bg-transparent'>
         <img src={assets.logo} alt=''/>
         <ul className='hidden md:flex gap-7 text-white'>
           <a href='#Header' className='cursor-pointer hover:text-gray-400'>Home</a>
